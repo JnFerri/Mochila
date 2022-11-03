eee9f30ed07bd42b42daee815d39560df85bbd37
Jean Ferri <jeanferrimh@gmail.com>
2022-11-03 18:26:15 -0300

    03/11/2022

--- js/main.js
+++ js/main.js
@@ -1,18 +1,44 @@
 const form = document.getElementById('novoItem')
 const lista = document.getElementById('lista')
+const inputNome = document.getElementById('nome')
+const inputQuantidade = document.getElementById('quantidade')
+var listaItens = []
+
+console.log(localStorage)
+
 form.addEventListener('submit',(evento) =>{
     evento.preventDefault()
     
     criarItem(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
-    
-})
+    localStorage.setItem('itens', JSON.stringify(listaItens))
+    limparCampos()
 
-function criarItem(nome, quantidade){
+    JSON.parse(localStorage.getItem('itens')).forEach((elemento) => function(){
     const novoItem = document.createElement('li')
-    novoItem.classList.add('item')
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
-    novoItem.appendChild(numeroItem) 
+    const numeroElemento = document.createElement('strong')
+    numeroElemento.innerHTML = elemento.quantidade
+    novoItem.appendChild(numeroElemento)
+    novoItem.innerHTML = elemento.nome
     lista.appendChild(novoItem)
     
+})})
+
+
+function criarItem(nome, quantidade){
+    const item = {
+        'nome':nome,
+        'quantidade':quantidade}
+
+        listaItens.push(item)
+        console.log(listaItens)
+    }
+    
+    
+   
+
+
+function limparCampos(){
+    inputNome.value = ''
+    inputQuantidade.value = ''
+    inputNome.focus()
 }
