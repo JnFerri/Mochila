9a7a46c92eb734ae8fec05b25c995cd5e700aebc
Jean Ferri <jeanferrimh@gmail.com>
2022-11-04 13:14:26 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ const lista = document.getElementById('lista')
 const inputNome = document.getElementById('nome')
 const inputQuantidade = document.getElementById('quantidade')
 var listaItens = []
-
-console.log(localStorage)
+var localStorageItens = []
 
 form.addEventListener('submit',(evento) =>{
     evento.preventDefault()
@@ -12,16 +11,11 @@ form.addEventListener('submit',(evento) =>{
     criarItem(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
     localStorage.setItem('itens', JSON.stringify(listaItens))
     limparCampos()
-
-    JSON.parse(localStorage.getItem('itens')).forEach((elemento) => function(){
-    const novoItem = document.createElement('li')
-    const numeroElemento = document.createElement('strong')
-    numeroElemento.innerHTML = elemento.quantidade
-    novoItem.appendChild(numeroElemento)
-    novoItem.innerHTML = elemento.nome
-    lista.appendChild(novoItem)
     
-})})
+    var itensLocalStorage = localStorage.getItem('itens')
+    updateElemento(JSON.parse(itensLocalStorage))
+    
+    })
 
 
 function criarItem(nome, quantidade){
@@ -30,10 +24,14 @@ function criarItem(nome, quantidade){
         'quantidade':quantidade}
 
         listaItens.push(item)
-        console.log(listaItens)
     }
     
-    
+function updateElemento(dados){
+   dados.forEach((elemento) => {
+        for(var i=0; i<dados.length;i++){
+       lista.innerHTML = `<li class='item'><strong>${elemento.quantidade}</strong>${elemento.nome}</li>`
+        }
+})}
    
 
 
